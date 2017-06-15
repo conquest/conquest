c81bee7f3f1ad3a96d5b2998ae017ba53fdff624
ze <zelkatani@gmail.com>
2017-06-15 13:17:47 -0400

    Initial commit

---
+++ core/src/com/zelkatani/conquest/Conquest.java
@@ -0,0 +1,29 @@
+package com.zelkatani.conquest;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Conquest extends ApplicationAdapter {
+	SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
---
+++ desktop/src/com/zelkatani/conquest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zelkatani.conquest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zelkatani.conquest.Conquest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Conquest(), config);
+	}
+}
