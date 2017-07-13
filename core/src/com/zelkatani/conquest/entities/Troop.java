1e6a8b23c904e7a3ef5ab9b3947c400645b4ee51
ze <zelkatani@gmail.com>
2017-07-13 15:17:49 +0100

    Make troops travel to their goal

--- core/src/com/zelkatani/conquest/entities/Troop.java
+++ core/src/com/zelkatani/conquest/entities/Troop.java
@@ -1,12 +1,18 @@
 package com.zelkatani.conquest.entities;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ai.pfa.DefaultGraphPath;
 import com.badlogic.gdx.ai.pfa.GraphPath;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Disposable;
@@ -15,7 +21,6 @@ import com.zelkatani.conquest.Assets;
 public class Troop extends Actor implements Disposable {
     private int troops;
     private GraphPath<Tile> path;
-    private Tile current;
 
     private Texture texture;
     private Label label;
@@ -31,14 +36,11 @@ public class Troop extends Actor implements Disposable {
         texture = Assets.TROOP;
         setSize(texture.getWidth(), texture.getHeight());
 
-        current = this.path.get(0);
-        setPosition(current.getX(Align.center) - getWidth() / 2, current.getY(Align.center) - getHeight() / 2);
-
         label = new Assets.ConquestLabel("" + this.troops, getX(), getY(), getWidth(), getHeight());
         label.setAlignment(Align.center, Align.center);
 
         setOrigin(Align.center);
-        setRotation(angleToNext());
+        createActions();
     }
 
     @Override
@@ -52,12 +54,56 @@ public class Troop extends Actor implements Disposable {
     }
 
     @Override
+    public void act(float delta) {
+        super.act(delta);
+        label.act(delta);
+    }
+
+    private Vector2 centered(Tile tile) {
+        return new Vector2(tile.getX(Align.center) - getWidth() / 2, tile.getY(Align.center) - getHeight() / 2);
+    }
+
+    private float speed(Tile now, Tile next) {
+        return MathUtils.clamp(centered(now).dst(centered(next)) * MathUtils.log(5, troops) / 25, 1, 5);
+    }
+
+    private void createActions() {
+        SequenceAction troopSequence = new SequenceAction();
+        SequenceAction labelSequence = new SequenceAction();
+
+        Vector2 centered = centered(path.get(0));
+        setPosition(centered.x, centered.y);
+        label.setPosition(centered.x, centered.y);
+
+        for (int i = 0; i < path.getCount() - 1; i++) {
+            Tile next = path.get(i + 1);
+            centered = centered(next);
+
+            troopSequence.addAction(Actions.rotateTo(angleToNext(i)));
+            troopSequence.addAction(Actions.moveTo(centered.x, centered.y, speed(path.get(i), next), Interpolation.linear));
+            labelSequence.addAction(Actions.moveTo(centered.x, centered.y, speed(path.get(i), next), Interpolation.linear));
+        }
+
+        troopSequence.addAction(Actions.run(() -> {
+            Tile last = path.get(path.getCount() - 1);
+            last.addTroops(troops);
+            last.updateLabel();
+        }));
+        troopSequence.addAction(Actions.removeActor());
+        labelSequence.addAction(Actions.removeActor());
+
+        addAction(troopSequence);
+        label.addAction(labelSequence);
+    }
+
+    @Override
     public void dispose() {
         texture.dispose();
     }
 
-    private float angleToNext() {
-        Tile next = path.get(1);
+    private float angleToNext(int index) {
+        Tile current = path.get(index);
+        Tile next = path.get(index + 1);
         float dX = current.getX(Align.center) - next.getX(Align.center);
         float dY = current.getY(Align.center) - next.getY(Align.center);
         return MathUtils.atan2(dY, dX) * MathUtils.radDeg;
